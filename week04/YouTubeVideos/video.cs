0f27b33b36c3e29121886a57a29ae59dc9f55221
gguzmanhnd <gegzocp@gmail.com>
2026-07-23 10:00:13 -0600

    week0401

--- week04/YouTubeVideos/video.cs
+++ week04/YouTubeVideos/video.cs
@@ -1,4 +1,4 @@
-using System
+using System;
 using System.Collections.Generic;
 
 public class Video
@@ -13,7 +13,7 @@ public class Video
         Title = title;
         Author = author;
         LengthInSeconds = lengthInSeconds;
-       
+    
     }
 
     public void AddComment(Comment comment)
