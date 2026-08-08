edcd193c6aa761c30a6a088c85cef6e71bc43ea2
gguzmanhnd <gegzocp@gmail.com>
2026-08-08 09:43:33 -0600

    week06_Goals

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,40 @@
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+        private int _bonus;
+
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+    {
+        _target = target;
+        _amountCompleted = 0;
+        _bonus = bonus;
+    }
+
+    public override int RecordEvent()
+    {
+        _amountCompleted++;
+        if (_amountCompleted >= _target)
+        {
+            return _points + _bonus;
+        }
+        else
+        {
+            return _points;
+        }
+    }
+
+    public override bool IsComplete() => _amountCompleted >= _target;
+    
+    public override string GetDetailsString()
+    {
+        return $"{base.GetDetailsString()} -- Currently completed: {_amountCompleted}/{_target}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal:{_shortName},{_description},{_points},{_bonus},{_target},{_amountCompleted}";
+    }
+}
+    
---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,21 @@
+public class GoalManager
+{
+    private List<Goal> goals;
+    private int _score;
+
+    public GoalManager()
+    {
+        goals = new List<Goal>();
+        _score = 0;
+    }
+
+    public void Start() { }
+    public void DisplayPlayerInfo() {}
+    public void ListGoalNames() {}
+    public void ListGoalDetails() {}
+    public void CreateGoal() {}
+    public void RecordEvent() {}
+    public void SaveGoals() {}
+    public void LoadGoals()  { }
+
+}
